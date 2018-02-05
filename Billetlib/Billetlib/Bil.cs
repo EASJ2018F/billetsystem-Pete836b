f41a18df54864cb82bc2eaec6439580e52d4dcbe
Peter Clausen <pete836b@edu.easj.dk>
2018-02-05 10:33:57 +0100

    opgave 1-3

---
+++ Billetlib/Billetlib/Bil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetlib
+{
+    public class Bil
+    {
+        private string _nummerplade;
+        private DateTime _dato;
+        private int _pris;
+        private string _køretøj;
+
+        public string Nummerplade
+        {
+            get { return _nummerplade; }
+            set { _nummerplade = Nummerplade; }
+            
+        }
+        //property
+        public DateTime Dato
+        {
+            get { return _dato; }
+            set { _dato = Dato; }
+        }
+
+        //methode
+        public int BilletPris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+}
---
+++ Billetlib/BilletlibTests/BilTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Billetlib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetlib.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void BilletPrisTest()
+        {
+            var bil = new Bil();
+            int result = bil.BilletPris();
+            Assert.AreEqual(240, result);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            var bil = new Bil();
+            string result = bil.Køretøj();
+            Assert.AreEqual(result, "Bil");
+        }
+    }
+
+    
+}
